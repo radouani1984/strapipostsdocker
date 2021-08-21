7082b80880d3ba4ec8db71f92bd1b4003d1c2b9c
ADOUANI RIADH <adouani.riadh@gmail.com>
2021-08-21 19:15:06 +0200

    add debug to frontend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'po-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'posts';
+  hello = () => {
+    console.log('hello world');
+  };
 }
